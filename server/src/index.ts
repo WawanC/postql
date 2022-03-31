ce17b09cb9b692643c89318c9e40b1901b07c1bb
WawanC <kurniawancristianto@gmail.com>
2022-03-31 20:11:49 +0700

    setup graphql server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello World");
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema, Query, Resolver } from "type-graphql";
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  hello() {
+    return "Hello World";
+  }
+}
+
+const main = async () => {
+  const app = express();
+  const schema = await buildSchema({ resolvers: [HelloResolver] });
+
+  const apolloServer = new ApolloServer({
+    schema: schema,
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(8000, () => {
+    console.log("Server listening on 8000...");
+  });
+};
+
+main();
